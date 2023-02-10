e04da1bb90d84cfb0692f48cc0b78de1c1963c94
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-02-10 12:03:37 -0700

    connecting tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,21 +5,24 @@ USE company_db;
 
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT PRIMARY KEY NOT NULL,
   emp_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT NOT NULL
+    REFERENCES role(id),
     manager_id INT NULLIF("manager_id", "")
+    REFERENCES employee(id)
 );
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
 const sequelize = require('./config/connection');
 const mysql = require("mysql2");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//  Middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
+// Query database
+db.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+  });
+
+  db.query('SELECT * FROM role', function (err, results) {
+    console.log(results);
+  });
+
+  db.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+  });
+
+// Response when input is incorrect
+app.use((req, res) => {
+    console.log(404).end();
+});
 
 
 // Turn the db connection on
