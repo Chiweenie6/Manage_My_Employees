5262151aa70c54757556c38553d21543d73f0d6c
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-02-12 20:46:41 -0700

    able to view all three tables

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const linkDB = mysql.createConnection(
+const connection = mysql.createConnection(
   {
-    host: 'localhost',
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
   },
   console.log("Link to company_db successful 👍"
 ));
 
-module.exports = linkDB;
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,105 +1,151 @@
-const linkDatabase = require("./config/connection");
+const linkDB = require("./config/connection");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
-linkDatabase.connect((err) => {
+linkDB.connect((err) => {
   if (err) {
     throw err;
-} else {
-  console.log("Let's Begin 😁. What would you like to do?")
-}
+  } else {
+    console.log("Let's Begin 😁.");
+  }
 });
 
-const whatToDo = () => {
-  const userOptions = () => {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "option",
-          message: "What would you like to do?",
-          choices: [
-            "Add a department",
-            "Add a role",
-            "Add an employee",
-            "Delete departments, roles and employees",
-            "View combined salaries by department",
-            "View all departments",
-            "View all employees",
-            "View all roles",
-            "View employees by department",
-            "View employees by manager",
-            "Update an employee role",
-            "Update employee managers",
-            "All Done 🛑"
-          ],
-        },
-      ])
-      .then((input) => {
-        switch (input.option) {
-          case "Add a department":
-            addADepartment();
-            break;
-          case "Add a role":
-            addARole();
-            break;
-          case "Add an employee":
-            addAnEmployee();
-            break;
-          case "Delete departments, roles and employees":
-            deleteThings();
-            break;
-          case "View all departments":
-            viewAllDepartments();
-            break;
-          case "View all employees":
-            viewAllEmployees();
-            break;
-          case "View all roles":
-            viewAllRoles();
-            break;
-          case "View combined salaries by department":
-            viewCombinedSalaries();
-            break;
-          case "View employees by department":
-            viewEmployeesByDepartment();
-            break;
-          case "View employees by manager":
-            viewEmployeesByManager();
-            break;
-          case "Update an employee role":
-            updateAnEmployeeRole();
-            break;
-          case "Update employee managers":
-            updateEmployeeManagers();
-            break;
-          case "All Done":
-            allDone();
-        }
-      });
-  };
-
-  const viewAllDepartments = () => {
-    const findAllDepartments = `SELECT department.id AS id,
-    dep_name as department from department`;
-    linkDatabase.promise().query(findAllDepartments, (err, res) => {
-      if (err) { 
-        throw err;
-      } else {
-      console.table(res);
-      whatToDo();
+const userOptions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "option",
+        message: "What would you like to do?",
+        choices: [
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Delete departments, roles and employees",
+          "View all departments",
+          "View all employees",
+          "View all roles",
+          "View combined salaries by department",
+          "View employees by department",
+          "View employees by manager",
+          "Update an employee role",
+          "Update employee managers",
+          "All Done 🛑",
+        ],
+      },
+    ])
+    .then((input) => {
+      switch (input.option) {
+        case "Add a department":
+          addADepartment();
+          break;
+        case "Add a role":
+          addARole();
+          break;
+        case "Add an employee":
+          addAnEmployee();
+          break;
+        case "Delete departments, roles and employees":
+          deleteThings();
+          break;
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View combined salaries by department":
+          viewCombinedSalaries();
+          break;
+        case "View employees by department":
+          viewEmployeesByDepartment();
+          break;
+        case "View employees by manager":
+          viewEmployeesByManager();
+          break;
+        case "Update an employee role":
+          updateAnEmployeeRole();
+          break;
+        case "Update employee managers":
+          updateEmployeeManagers();
+          break;
+        case "All Done":
+          allDone();
       }
     });
-  };
-  userOptions();
 };
 
+const addADepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "newDep",
+        message: "Input new department name?",
+        validate: (input) => {
+          if (input === "") {
+            return "Must enter valid department";
+          }
+          return true;
+        },
+      },
+    ])
+    .then((answer) => {
+      let tableInfo = `INSERT INTO department (dep_name) VALUES (?)`;
+      linkDB.query(tableInfo, answer.newDep, (err, res) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log(answer.newDep + res);
+        }
+      });
+      userOptions();
+    });
+};
 
+const addARole = () => {
+  
+};
 
+const addAnEmployee = () => {};
 
+const deleteThings = () => {};
 
+const viewAllDepartments = () => {
+  linkDB.query("SELECT * FROM department", (err, res) => {
+    console.table(res);
+    userOptions();
+  });
+};
 
+const viewAllEmployees = () => {
+  linkDB.query("SELECT * FROM employee", (err, res) => {
+    console.table(res);
+    userOptions();
+  })
+};
 
+const viewAllRoles = () => {
+  linkDB.query("SELECT * FROM company_role", (err, res) => {
+    console.table(res);
+    userOptions();
+  })
+};
 
-whatToDo();
+const viewCombinedSalaries = () => {};
+
+const viewEmployeesByDepartment = () => {};
+
+const viewEmployeesByManager = () => {};
+
+const updateAnEmployeeRole = () => {};
+
+const updateEmployeeManagers = () => {};
+
+const allDone = () => {};
+
+userOptions();
